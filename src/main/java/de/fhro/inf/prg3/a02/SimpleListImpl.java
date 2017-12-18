b8803c98a76cff5cdf9ea446ee88fa67f2aaeea1
sINFmihint <michael.hintermeier@stud.fh-rosenheim.de>
2017-12-18 14:18:28 +0100

    add all the other methods and inner classes

--- src/main/java/de/fhro/inf/prg3/a02/SimpleListImpl.java
+++ src/main/java/de/fhro/inf/prg3/a02/SimpleListImpl.java
@@ -1,14 +1,19 @@
 package de.fhro.inf.prg3.a02;
 
+
+import org.w3c.dom.html.HTMLDListElement;
+
+import java.util.Iterator;
+
 /**
  * @author Peter Kurfer
  * Created on 10/6/17.
  */
-public class SimpleListImpl implements SimpleList {
+public class SimpleListImpl implements SimpleList, Iterable<Object> {
 
     //ATTRIBUTES
 
-    private Element head;
+    private ListElement head;
     private int size;
 
 
@@ -45,8 +50,98 @@ public class SimpleListImpl implements SimpleList {
         size++;
     }
 
-    public SimpleList filter(SimpleFilter filter) {
+    /**
+     * @return size of the list
+     */
+    public int size() {
+        return size;
+    }
 
+    /**
+     * Get a new SimpeList instance with all items of this list which match the given filter
+     *
+     * @param filter SimpleFilter instance
+     * @return new SimpleList instance
+     */
+    public SimpleList filter(SimpleFilter filter) {
+        SimpleList result = new SimpleListImpl();
+        for (Object o : this) {
+            if (filter.include(o)) {
+                result.add(o);
+            }
+        }
+        return result;
+
+    }
+
+    /**
+     * @inheritDoc
+     */
+    @Override
+    public Iterator<Object> iterator() {
+        return new SimpleIterator();
+    }
+
+    private class SimpleIterator implements Iterator<Object> {
+
+        private ListElement current = head;
+
+        /**
+         * @inheritDoc
+         */
+        @Override
+        public boolean hasNext() {
+            return current != null;
+        }
+
+        /**
+         * @inheritDoc
+         */
+        @Override
+        public Object next() {
+            Object tmp = current.getItem();
+            current = current.getNext();
+            return tmp;
+        }
+
+    }
+
+    /**
+     * Helper class for the linked list
+     * can be static because the ListElement does not need to access the SimpleList instance
+     */
+    private static Class ListElement
+
+    {
+        private Object item;
+        private ListElement next;
+
+        ListElement(Object item) {
+        this.item = item;
+        this.next = null;
+    }
+
+        /**
+         * @return get object in the element
+         */
+        public Object getItem () {
+        return item;
+    }
+
+        /**
+         * @return successor of the ListElement - may be NULL
+         */
+        public ListElement getNext () {
+        return next;
+    }
+
+        /**
+         * Sets the successor of the ListElement
+         * @param next ListElement
+         */
+        public void setNext () {
+        this.next = next;
+    }
     }
 
 
