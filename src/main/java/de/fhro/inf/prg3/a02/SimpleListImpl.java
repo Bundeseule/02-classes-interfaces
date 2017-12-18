43115cc72f98e771a6709334cb223efbf8d8c8b5
sINFmihint <michael.hintermeier@stud.fh-rosenheim.de>
2017-12-18 13:47:16 +0100

    completed method add

--- src/main/java/de/fhro/inf/prg3/a02/SimpleListImpl.java
+++ src/main/java/de/fhro/inf/prg3/a02/SimpleListImpl.java
@@ -18,6 +18,33 @@ public class SimpleListImpl implements SimpleList {
         head = null;
     }
 
+    /**
+     * Add on object to the end of the list
+     *
+     * @param item item to add
+     */
+    public void add(Object item) {
+        /*
+        special case empty list
+        if this case is true, add creates a new list by giving head a new ListElement
+         */
+        if (head == null) {
+            head = new ListElement(item);
+        } else {
+            /*
+            any other list strength
+            current list element is head
+            if there are next items in the list, this item is head until the head is at the end
+             */
+            ListElement current = head;
+            while (current.getNext() != null) {
+                current = current.getNext();
+            }
+            current.setNext(new ListElement(item));
+        }
+        size++;
+    }
+
     public SimpleList filter(SimpleFilter filter) {
 
     }
